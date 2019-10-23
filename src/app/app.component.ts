c24462792fb124105288df4f4233eae2676b2a87
letizia obiglio <letizia.obiglio@gmail.com>
2019-10-23 18:02:41 +0200

    feat: ngOninit filled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { TicketService } from './services/ticket.service';
+import { Component, OnInit } from "@angular/core";
+import { TicketService } from "./services/ticket.service";
+import { Ticket } from "./models/ticket";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
-  title = 'ticket-tracker';
+  title = "ticket-tracker";
+  tickets: Ticket[];
 
   constructor(private ticketService: TicketService) {}
 
   ngOnInit(): void {
     this.ticketService.getAll().subscribe((tickets) => {
-      console.log(tickets);
+      this.tickets = tickets;
     });
   }
 }
