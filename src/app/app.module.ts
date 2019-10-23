8e211750e6fec01a7748c225380e69031d51bf04
letizia obiglio <letizia.obiglio@gmail.com>
2019-10-23 15:42:54 +0200

    feat: component avatar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./pages/home/home.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { UserComponent } from './pages/user/user.component';
-
+import { UserComponent } from "./pages/user/user.component";
+import { AvatarComponent } from "./pages/user/avatar/avatar.component";
 @NgModule({
-  declarations: [AppComponent, HomeComponent, UserComponent],
+  declarations: [AppComponent, HomeComponent, UserComponent, AvatarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
