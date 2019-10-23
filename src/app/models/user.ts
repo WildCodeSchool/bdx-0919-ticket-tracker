f5a50d2d0515096d7ec01cfd412968d5ddcedcd8
letizia obiglio <letizia.obiglio@gmail.com>
2019-10-23 11:25:38 +0200

    build: user class filled

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,2 +1,10 @@
 export class User {
+  public id: number;
+  public firstName: string;
+  public lastName: string;
+  public avatar: string;
+
+  constructor(input: User) {
+    Object.assign(this, input);
+  }
 }
