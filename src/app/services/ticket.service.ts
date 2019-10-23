ddcef76d6943b10ca1f17e999168e252c7fc3a74
letizia obiglio <letizia.obiglio@gmail.com>
2019-10-23 17:55:51 +0200

    feat: getAll completed

--- src/app/services/ticket.service.ts
+++ src/app/services/ticket.service.ts
@@ -1,19 +1,27 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Ticket } from "../models/ticket";
+import { map } from "rxjs/operators";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TicketService {
-  static URL = 'https://wild-api.witpoc.com/tickets';
+  static URL = "https://wild-api.witpoc.com/tickets";
 
   constructor(private http: HttpClient) {}
 
-  getAll() {
-    return this.http.get(TicketService.URL);
+  getAll(): Observable<Ticket[]> {
+    return this.http.get(TicketService.URL).pipe(
+      map((tickets: any[]) => {
+        return tickets.map((ticket) => {
+          return new Ticket(ticket);
+        });
+      })
+    );
   }
 
   getById(id: number) {
-    return this.http.get(TicketService.URL + '/' + id);
+    return this.http.get(TicketService.URL + "/" + id);
   }
 }
