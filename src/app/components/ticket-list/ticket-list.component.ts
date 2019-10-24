8965bffd6242596a8bd7d91061c51dcd654a1828
Simon Piquet <piquet.simon@yahoo.fr>
2019-10-24 18:35:54 +0200

    Feat:Date

--- src/app/components/ticket-list/ticket-list.component.ts
+++ src/app/components/ticket-list/ticket-list.component.ts
@@ -7,7 +7,24 @@ import { Ticket } from "src/app/models/ticket";
   styleUrls: ["./ticket-list.component.scss"]
 })
 export class TicketListComponent implements OnInit {
-  tickets: Ticket[];
+  tickets: Ticket[] = [
+    {
+      id: 1,
+      title: "Bloqué sur AngularMaterial",
+      description:
+        "J'ai initié un projet en utilisant Angular Material.Quand j'essaie de créer un avavar, cela ne fonctionne pas.",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      id: 2,
+      title: "Plus de papier toilette",
+      description: "En galère",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ];
+
   constructor() {}
 
   ngOnInit() {}
