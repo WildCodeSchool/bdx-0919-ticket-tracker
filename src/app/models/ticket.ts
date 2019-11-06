4cfdbcfbcc4ea2876913d4d648c39434b52f723b
letizia obiglio <letizia.obiglio@gmail.com>
2019-11-06 15:39:46 +0100

    feat: user name dynamique

--- src/app/models/ticket.ts
+++ src/app/models/ticket.ts
@@ -1,9 +1,11 @@
+import { User } from "src/app/models/user";
 export class Ticket {
   public id?: number;
   public title: string;
   public description: string;
   public createdAt: Date;
   public updatedAt: Date;
+  public user: User;
 
   constructor(input: Ticket) {
     Object.assign(this, input);
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,7 +1,7 @@
 export class User {
   public id: number;
-  public firstName: string;
-  public lastName: string;
+  public firstname: string;
+  public lastname: string;
   public avatar: string;
   public status: string;
 
