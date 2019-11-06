1e2b95f778ce39c34455a4a478b09309067421e3
Delache <delache33@gmail.com>
2019-11-06 15:53:52 +0100

    chore: deportedService

--- src/app/services/ticket.service.ts
+++ src/app/services/ticket.service.ts
@@ -1,3 +1,4 @@
+import { WsHelperService } from './ws-helper.service';
 import { User } from 'src/app/models/user';
 
 import { Injectable } from '@angular/core';
@@ -13,38 +14,35 @@ export class TicketService {
   static URL = 'https://wild-api.witpoc.com/tickets-secure';
   private token: string;
 
-  constructor(private http: HttpClient) {}
+  constructor(private wshelper: WsHelperService) {}
 
-  setToken(token: string) {
-    this.token = token;
-  }
 
   public getAll(): Observable<Ticket[]> {
-    const headers =  new HttpHeaders({Authorization: 'Bearer ' + this.token});
-
-    return this.http
-      .get(TicketService.URL, {headers})
+    return this.wshelper
+      .get(TicketService.URL)
       .pipe(map(this.convertDataFromServerToTickets));
   }
 
   public getById(id: number) {
-    return this.http
+    return this.wshelper
       .get(TicketService.URL + '/' + id)
       .pipe(map(this.convertDataFromServerToTickets));
   }
 
   public createTicket(ticket: Ticket): Observable<any> {
     ticket.user = ({id: 4} as User);
-    return this.http.post(TicketService.URL, ticket);
+    return this.wshelper.post(TicketService.URL, ticket);
   }
 
   private convertDataFromServerToTickets(tickets: any[]): Ticket[] {
+    const headers =  new HttpHeaders({Authorization: 'Bearer ' + this.token});
     return tickets.map((ticket) => {
       return new Ticket(ticket);
     });
   }
 
   deleteTicket(id: number): Observable<any> {
-    return this.http.delete(TicketService.URL + `/${id}` );
+    const headers =  new HttpHeaders({Authorization: 'Bearer ' + this.token});
+    return this.wshelper.delete(TicketService.URL + `/${id}` );
   }
 }
