ca71f55f137006cb4c9b9203dc93857aff6ea79c
letizia obiglio <letizia.obiglio@gmail.com>
2019-11-07 11:14:14 +0100

    fix: groups instead of group

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,38 +1,35 @@
-import { Group } from './../../models/group';
-import { Ticket } from 'src/app/models/ticket';
-import { TicketService } from './../../services/ticket.service';
-import { Component, OnInit, Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
+import { Group } from "./../../models/group";
+import { Ticket } from "src/app/models/ticket";
+import { TicketService } from "./../../services/ticket.service";
+import { Component, OnInit, Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  selector: "app-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./form.component.scss"]
 })
 export class FormComponent implements OnInit {
   constructor(private ticketService: TicketService, private router: Router) {}
   newTicket: Ticket = new Ticket();
   ticketType: string;
 
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
   onReset(createTicket: NgForm) {
     createTicket.resetForm();
   }
   onFormSubmit(newTicket: Ticket) {
-    if (this.ticketType === 'CURSUS') {
-      newTicket.group = { id: 178} as Group;
+    if (this.ticketType === "CURSUS") {
+      newTicket.groups = { id: 178 } as Group;
     } else {
-      newTicket.school = { id: 5};
+      newTicket.school = { id: 5 };
     }
-    this.ticketService.createTicket
-    (newTicket).subscribe(() => {
-    this.router.navigate(['/user']);
+    this.ticketService.createTicket(newTicket).subscribe(() => {
+      this.router.navigate(["/user"]);
     });
   }
   onClose() {
-    this.router.navigate(['/user']);
+    this.router.navigate(["/user"]);
   }
 }
