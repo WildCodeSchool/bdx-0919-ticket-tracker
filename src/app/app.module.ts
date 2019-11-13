14dee43f2d2c17d669f4c08ffab1a284af6fc605
Delache <delache33@gmail.com>
2019-11-13 20:48:53 +0100

    feat: addHeaderOnForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,13 +17,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserComponent } from './pages/user/user.component';
-import { AvatarComponent } from './pages/user/avatar/avatar.component';
+import { AvatarComponent } from './components/avatar/avatar.component';
 import { TabComponent } from './pages/user/tab/tab.component';
 import { TicketListComponent } from './components/ticket-list/ticket-list.component';
 import { FormComponent } from './pages/form/form.component';
 import { TicketComponent } from './components/ticket/ticket.component';
 import { AuthComponent } from './pages/auth/auth.component';
 import { MatTabsModule } from '@angular/material/tabs';
+import { HeaderComponent } from './pages/user/header/header.component';
+import { FooterComponent } from './pages/user/footer/footer.component';
+import { SinglePageComponent } from './pages/single-page/single-page.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +40,10 @@ import { MatTabsModule } from '@angular/material/tabs';
     TicketComponent,
     AuthComponent,
     EditButtonComponent,
-    AdminButtonComponent
+    AdminButtonComponent,
+    HeaderComponent,
+    FooterComponent,
+    SinglePageComponent
   ],
   imports: [
     BrowserModule,
