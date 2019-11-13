3d514501ace6c429800eccc78f53ba99e7dba218
Fadi Rata <fadi.rata@live.fr>
2019-11-13 11:04:20 +0100

    feat: Suppression fini

--- src/app/components/ticket/edit-button/edit-button.component.ts
+++ src/app/components/ticket/edit-button/edit-button.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
-import { Ticket } from "src/app/models/ticket";
-import { TicketService } from "src/app/services/ticket.service";
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Ticket } from '../../../models/ticket';
+import { TicketService } from '../../../services/ticket.service';
 
 @Component({
-  selector: "app-edit-button",
-  templateUrl: "./edit-button.component.html",
-  styleUrls: ["./edit-button.component.scss"]
+  selector: 'app-edit-button',
+  templateUrl: './edit-button.component.html',
+  styleUrls: ['./edit-button.component.scss']
 })
 export class EditButtonComponent implements OnInit {
   @Input() ticket: Ticket;
--- src/app/components/ticket/ticket.component.ts
+++ src/app/components/ticket/ticket.component.ts
@@ -1,19 +1,27 @@
-import { Ticket } from "src/app/models/ticket";
-import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import { Ticket } from './../../models/ticket';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
-  selector: "app-ticket",
-  templateUrl: "./ticket.component.html",
-  styleUrls: ["./ticket.component.scss"]
+  selector: 'app-ticket',
+  templateUrl: './ticket.component.html',
+  styleUrls: ['./ticket.component.scss']
 })
 export class TicketComponent implements OnInit {
-  @Input() ticket: Ticket;
 
-  tickets: Ticket[];
 
   constructor() {}
+  @Input() ticket: Ticket;
+  @Output() getCheckDelete = new EventEmitter<Ticket>();
+  tickets: Ticket[];
+
+
 
   ngOnInit() {
     return this.ticket;
+  }
+
+  onCheckDelete($event) {
+    this.getCheckDelete.emit($event);
   }
 }
